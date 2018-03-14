c358d50de0bd56a426d507b94bd6e99d4bfda39d
Administrator <congchao0313@163.com>
2018-03-15 07:40:04 +0800

    0315home

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="keywords" content="Hot Restaurant">
+	<meta name="description" content="Hot Restaurant">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My Restaurant</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+
+<header>
+	<h1> Hot Restaurant </h1>
+</header>
+
+<!-- nav -->
+<nav>
+	<ul>
+		<li><a href="location.php">Location </a></li>
+		<li><a href="contact.php" class="on">Contact</a></li>
+		<li><a href="specials.php">Specials </a></li>
+		<li><a href="menu.php">Menu</a></li>
+		<li><a href="index.php" >Home</a></li>
+	</ul>
+	<div class="clear"></div>
+</nav>
+<!-- banner -->
+<div class="banner">
+	<div class="img"><img src="images/banner1.jpg" height="450" width="960" /></div>
+</div>
+<p class="exp">Welcome to Hot Restaurant</p>
+
+<div class="amb">
+	<p>My restaurant is called Hot Restaurant.
+		We have the excellent cooks to provide you with extremely delicious food.Our style is Chinese food.
+		We also have the outstanding service.We graruntee you will enjoy your meal very much in our restaurant.
+		Customers who come on Tuesday will have the half price.
+		Welcome to our restaurant!	</p>
+</div>
+
+<!-- footer -->
+<footer>
+	<p>Contact Me:042*****　Email:****@qq.com　copyright@2017 by ***</p>
+</footer>
+</body>
+</html>
