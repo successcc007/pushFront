1a25117ab43963dc55b9cbaa31da9affa7514f9f
Administrator <congchao0313@163.com>
2018-03-16 07:18:02 +0800

    0316 am home

--- js/Jsindex.js
+++ js/Jsindex.js
@@ -3,8 +3,11 @@
  */
 
 const URLBASE = "http://localhost/jishiback/api/v1";
+
 $(document).ready(function () {
     countryInit();
+    SchangeCountry();
+    SchangeProvince();
 });
 
 /*获取国家*/
@@ -13,24 +16,58 @@ function countryInit() {
     var submitData = {
         type: "country"
     };
-    ajaxRequest(url, submitData, callbackcountry, "GET");
+    ajaxRequest(url, submitData, CalbGetCountry, "GET");
 }
 
-/* 国家数据回调*/
-function callbackcountry(result) {
+/* 国家数据回调，国家数据填充*/
+function CalbGetCountry(result) {
     var len = result['info'].length;
     var select = $(".country select");
     for (var i = 0; i < len; i++) {
-        select.options.add(result['info'][i]['s_name'], result['info'][i]['s_name']);
+        select.options.add(result['info'][i]['i_id'], result['info'][i]['s_name']);
     }
 }
 
-/*国家、省、市的change事件*/
-function SelectChange(){
+/*国家的change事件*/
+function SchangeCountry(){
     $(".country select").change(function(){
-//todu get province
+        var sOption = $(this).children('option:selected').val();
+        var url = URLBASE + "/index/select/province";
+        var submitData = {
+            type: "province",
+            province_id: sOption
+        };
+        ajaxRequest(url, submitData, CalbCountryClick, "GET");
     });
 }
 
+/* 国家change回调,省数据填充*/
+function CalbCountryClick(result) {
+    var len = result['info'].length;
+    var select = $(".province select");
+    for (var i = 0; i < len; i++) {
+        select.options.add(result['info'][i]['i_id'], result['info'][i]['s_name']);
+    }
+}
 
+/*省的change事件*/
+function SchangeProvince(){
+    $(".province select").change(function(){
+        var sOption = $(this).children('option:selected').val();
+        var url = URLBASE + "/index/select/city";
+        var submitData = {
+            type: "city",
+            province_id: sOption
+        };
+        ajaxRequest(url, submitData, CalbProvinceClick, "GET");
+    });
+}
 
+/* 省change回调,市数据填充*/
+function CalbProvinceClick(result) {
+    var len = result['info'].length;
+    var select = $(".city select");
+    for (var i = 0; i < len; i++) {
+        select.options.add(result['info'][i]['i_id'], result['info'][i]['s_name']);
+    }
+}
